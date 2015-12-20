ebc6482e75cd939bd1913d757d90ef9898085a5a
helpfulrobot <chris@silverstripe.com>
2015-12-20 17:23:13 +1300

    Converted to PSR-2

--- code/IGeocodingService.php
+++ code/IGeocodingService.php
@@ -3,37 +3,38 @@
 /**
  * @package geocoding
  */
-interface IGeocodingService {
-	
-	/**
-	 * Is this service over the daily limit?
-	 * 
-	 * @return boolean
-	 */
-	public function isOverLimit();
-	
-	/**
-	 * Returns resulting geocode in the format
-	 * 
-	 * array(
-	 *    'Success' => true,
-	 *    'Latitude' => '0.000',
-	 *	  'Longitude' => '0.000',
-	 *	  'Error' => '', // error code
-	 *    'Message' => '' // error message
-	 * )
-	 * 
-	 * @param string $address
-	 * @return array Result
-	 */
-	public function geocode($address);
-	
-	
-	/**
-	 * Cleans up an address for geocoding
-	 * 
-	 * @param string|array $address
-	 * @return string
-	 */
-	public function normaliseAddress($address);
+interface IGeocodingService
+{
+    
+    /**
+     * Is this service over the daily limit?
+     * 
+     * @return boolean
+     */
+    public function isOverLimit();
+    
+    /**
+     * Returns resulting geocode in the format
+     * 
+     * array(
+     *    'Success' => true,
+     *    'Latitude' => '0.000',
+     *	  'Longitude' => '0.000',
+     *	  'Error' => '', // error code
+     *    'Message' => '' // error message
+     * )
+     * 
+     * @param string $address
+     * @return array Result
+     */
+    public function geocode($address);
+    
+    
+    /**
+     * Cleans up an address for geocoding
+     * 
+     * @param string|array $address
+     * @return string
+     */
+    public function normaliseAddress($address);
 }
